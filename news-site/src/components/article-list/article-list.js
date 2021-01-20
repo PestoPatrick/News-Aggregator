022af702dfbe1bbd3b9a84fcc7ec21fb5abed12f
patrick <paddymcpherson@hotmail.com>
2021-01-20 22:38:09 +0000

    I need to make a backend for this news aggregator site as the newsapi has CORS issues and I need to handle that on the backend and then direct that to the front end.

--- news-site/src/components/article-list/article-list.js
+++ news-site/src/components/article-list/article-list.js
@@ -5,6 +5,8 @@ import {Article} from "../articles/article";
 
 export class ArticleList extends React.Component {
 
+    //I think i will try using Axios instead as well
+
     componentDidMount() {
         const apikey = 'https://newsapi.org/v2/everything&apiKey=1bb5eaf19afb4fdbaf06ca6028ab6355';
         fetch(apikey)
