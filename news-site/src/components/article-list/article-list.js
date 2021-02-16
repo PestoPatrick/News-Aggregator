6e30e974685693a6e290c75c4915312768b604cf
patrick <paddymcpherson@hotmail.com>
2021-02-16 17:05:14 +0000

    Completely need to take a step back and start again. Need to learn how to use React hooks as this seems to be becoming the standard. Going to follow a tutorial before trying to do this again. I will probably use anothe branch or repo and delete this one when I try the news api again.

--- news-site/src/components/article-list/article-list.js
+++ news-site/src/components/article-list/article-list.js
@@ -1,27 +1,36 @@
 import React from 'react'
 import './article-list.scss'
 import {Article} from "../articles/article";
+import {useState, useEffect} from 'react';
 
 
-export class ArticleList extends React.Component {
+//Transforming these classes into functions and using react hooks
+
+export function ArticleList() {
+    const [data,setData] = useState([])
 
     //I think i will try using Axios instead as well
-
-    componentDidMount() {
-        const apikey = 'https://newsapi.org/v2/everything&apiKey=1bb5eaf19afb4fdbaf06ca6028ab6355';
-        fetch(apikey)
-            .then((response) => response.json())
-            .then((data) => console.log('This is your data', data));
+    async function getData() {
+        const apikey = 'https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15';
+        const response = await fetch(apikey)
+        const data = await response.json();
+        setData(data);
     }
 
-    render() {
+    useEffect(() => {
+        getData();
+    }, [])
+
+    return data
+
         return (
             <div>
-            <Article/>
+                {data.map((el) => (
+                    <li key={el.id}>{el.title}</li>
+                ))}
             <h1>Check the console</h1>
             </div>
         )
-    }
 }
 
 /* how to dynamically render components:
